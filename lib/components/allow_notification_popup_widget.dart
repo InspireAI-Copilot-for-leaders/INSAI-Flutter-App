e4d5f6bf18e886b832e6ef937557b70a85c655db
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-02 17:16:11 +0000

    push notification action

---
+++ lib/components/allow_notification_popup_widget.dart
@@ -0,0 +1,209 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'allow_notification_popup_model.dart';
+export 'allow_notification_popup_model.dart';
+
+class AllowNotificationPopupWidget extends StatefulWidget {
+  const AllowNotificationPopupWidget({
+    super.key,
+    required this.allowAction,
+    required this.denyAction,
+  });
+
+  final Future Function()? allowAction;
+  final Future Function()? denyAction;
+
+  @override
+  State<AllowNotificationPopupWidget> createState() =>
+      _AllowNotificationPopupWidgetState();
+}
+
+class _AllowNotificationPopupWidgetState
+    extends State<AllowNotificationPopupWidget> {
+  late AllowNotificationPopupModel _model;
+
+  @override
+  void setState(VoidCallback callback) {
+    super.setState(callback);
+    _model.onUpdate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => AllowNotificationPopupModel());
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _model.maybeDispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    context.watch<FFAppState>();
+
+    return Align(
+      alignment: AlignmentDirectional(0.0, 0.0),
+      child: Padding(
+        padding: EdgeInsetsDirectional.fromSTEB(32.0, 0.0, 32.0, 0.0),
+        child: Container(
+          width: MediaQuery.sizeOf(context).width * 1.0,
+          height: 400.0,
+          decoration: BoxDecoration(
+            color: FlutterFlowTheme.of(context).secondaryBackground,
+            borderRadius: BorderRadius.circular(40.0),
+          ),
+          alignment: AlignmentDirectional(0.0, -1.0),
+          child: Align(
+            alignment: AlignmentDirectional(0.0, -1.0),
+            child: Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(0.0, 24.0, 0.0, 24.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/notification.png',
+                    width: 64.0,
+                    height: 64.0,
+                    fit: BoxFit.contain,
+                    alignment: Alignment(0.0, 0.0),
+                  ),
+                  Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        'Get notified!',
+                        style: FlutterFlowTheme.of(context)
+                            .headlineLarge
+                            .override(
+                              fontFamily: 'Poppins',
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.w800,
+                              useGoogleFonts:
+                                  GoogleFonts.asMap().containsKey('Poppins'),
+                            ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(
+                            24.0, 0.0, 24.0, 0.0),
+                        child: Text(
+                          'We will send you when content is ready and new things come up.',
+                          textAlign: TextAlign.center,
+                          style: FlutterFlowTheme.of(context)
+                              .bodyLarge
+                              .override(
+                                fontFamily: 'Poppins',
+                                useGoogleFonts:
+                                    GoogleFonts.asMap().containsKey('Poppins'),
+                                lineHeight: 1.5,
+                              ),
+                        ),
+                      ),
+                    ].divide(SizedBox(height: 8.0)),
+                  ),
+                  Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(
+                            24.0, 0.0, 24.0, 0.0),
+                        child: FFButtonWidget(
+                          onPressed: () async {
+                            logFirebaseEvent(
+                                'ALLOW_NOTIFICATION_POPUP_ALLOW_NOTIFICAT');
+                            logFirebaseEvent('Button_execute_callback');
+                            await widget.allowAction?.call();
+                          },
+                          text: 'Allow Notifications',
+                          options: FFButtonOptions(
+                            width: MediaQuery.sizeOf(context).width * 1.0,
+                            height: 64.0,
+                            padding: EdgeInsetsDirectional.fromSTEB(
+                                24.0, 0.0, 24.0, 0.0),
+                            iconPadding: EdgeInsetsDirectional.fromSTEB(
+                                0.0, 0.0, 0.0, 0.0),
+                            color: FlutterFlowTheme.of(context).primary,
+                            textStyle: FlutterFlowTheme.of(context)
+                                .titleSmall
+                                .override(
+                                  fontFamily: FlutterFlowTheme.of(context)
+                                      .titleSmallFamily,
+                                  color: FlutterFlowTheme.of(context).alternate,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w600,
+                                  useGoogleFonts: GoogleFonts.asMap()
+                                      .containsKey(FlutterFlowTheme.of(context)
+                                          .titleSmallFamily),
+                                ),
+                            elevation: 0.0,
+                            borderSide: BorderSide(
+                              color: Colors.transparent,
+                              width: 1.0,
+                            ),
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(
+                            24.0, 0.0, 24.0, 0.0),
+                        child: FFButtonWidget(
+                          onPressed: () async {
+                            logFirebaseEvent(
+                                'ALLOW_NOTIFICATION_POPUP_MAYBE_LATER_BTN');
+                            logFirebaseEvent('Button_execute_callback');
+                            await widget.denyAction?.call();
+                          },
+                          text: 'Maybe later',
+                          options: FFButtonOptions(
+                            width: MediaQuery.sizeOf(context).width * 1.0,
+                            height: 64.0,
+                            padding: EdgeInsetsDirectional.fromSTEB(
+                                24.0, 0.0, 24.0, 0.0),
+                            iconPadding: EdgeInsetsDirectional.fromSTEB(
+                                0.0, 0.0, 0.0, 0.0),
+                            color: Color(0x00FFFFFF),
+                            textStyle: FlutterFlowTheme.of(context)
+                                .titleSmall
+                                .override(
+                                  fontFamily: FlutterFlowTheme.of(context)
+                                      .titleSmallFamily,
+                                  color: Color(0xFF111111),
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w600,
+                                  useGoogleFonts: GoogleFonts.asMap()
+                                      .containsKey(FlutterFlowTheme.of(context)
+                                          .titleSmallFamily),
+                                ),
+                            elevation: 0.0,
+                            borderSide: BorderSide(
+                              color: Colors.transparent,
+                              width: 1.0,
+                            ),
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ].divide(SizedBox(height: 24.0)),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
