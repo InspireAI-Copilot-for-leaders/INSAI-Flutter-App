94b0ed888bef733b3c351b81e76d60866e0296e1
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-05-05 16:53:02 +0000

    Updating to latest FlutterFlow output.

--- lib/home/home_widget.dart
+++ lib/home/home_widget.dart
@@ -828,17 +828,6 @@ class _HomeWidgetState extends State<HomeWidget> with TickerProviderStateMixin {
                                                                     ?.broadDomains
                                                                     ?.toList() ??
                                                                 []))
-                                                        .where(
-                                                          'scrapped_at',
-                                                          isGreaterThanOrEqualTo:
-                                                              functions
-                                                                  .modifiedDateTime(
-                                                                      getCurrentTimestamp,
-                                                                      0,
-                                                                      0,
-                                                                      4,
-                                                                      false),
-                                                        )
                                                         .orderBy('scrapped_at',
                                                             descending: true),
                                               ),
